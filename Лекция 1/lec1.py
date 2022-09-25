d9f21e317e5d31ccbe375321949487a732058318
AlexHeilong <alexdeev@mail.ru>
2022-09-25 20:13:09 +0800

    initial commit added

---
+++ Лекция 1/lec1.py
@@ -0,0 +1,30 @@
+from xmlrpc.client import boolean
+
+
+print('Hello World')
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+# value = None
+# print(type(value))
+
+# print(type(a))
+# print(type(b))
+# value = 1234
+# print(type(value))
+
+a = 123
+b = 12.3
+s = 'hello \nworld' # вывод строки
+print(s)
+print(a, b, s)
+print(a, '-',b, '-', s)
+print('{} - {} - {}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+f = True
+print(f)
+
+list = ['1', '2', '3', 'Hello, World', 1, 2, 3.5, True]
+print(list)
